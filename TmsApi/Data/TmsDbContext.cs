1848f26d0a373518ca733770666295f8d93ec75f
merontekle27 <merontekle0727@gmail.com>
2026-08-20 11:08:57 +0300

    Enhance comments in TmsDbContext and Student models for better clarity

--- TmsApi/Data/TmsDbContext.cs
+++ TmsApi/Data/TmsDbContext.cs
@@ -1,10 +1,12 @@
+//import all the Ef classes like DbContext,DbSet,migration,Modelbuilder..
 using Microsoft.EntityFrameworkCore;
 using TmsApi.Models;
 
 namespace TmsApi.Data;
 
-public class TmsDbContext : DbContext
+public class TmsDbContext : DbContext //our database context inherits all the  func of EF Core's DbContext
 {
+    //this constructor tells when creating this context, use the db config provided in program.cs
     public TmsDbContext(DbContextOptions<TmsDbContext> options)
         : base(options)
     {
--- TmsApi/Models/Student.cs
+++ TmsApi/Models/Student.cs
@@ -1,7 +1,10 @@
 namespace TmsApi.Models;
 public class Student
 {
+// ef core recognizes a property named Id as the primary key
     public int Id{get; set; }
+    //In short: = string.Empty makes the property 
+    // start as a valid blank value instead of a possibly null value.
     public string RegistrationNumber { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public decimal GPA { get; set; }
